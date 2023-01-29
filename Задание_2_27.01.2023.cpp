f08c03cb5df55105f0a48ea14bd6c98eafb60534
Георгий Беляев <georgijbelyaev2@gmail.com>
2023-01-29 21:52:37 +0300

    Create Задание_2_27.01.2023.cpp

---
+++ Задание_2_27.01.2023.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+  std::cout<<"Задание 1\n\n";
+  std::cout << "Я не могу избавиться от боли\nМне надоело здесь играть, я не хочу быть в роли\nИ все что я имею словно ничего не стоит\nИ когда ты уйдёшь, кто тебя вспомнит?\n\n THRILL PILL\n\n";
+  std::cout<<"Задание 2\n\n";
+  std::cout<<"Every\n\tHunter\n\t\tWants\n\t\t\tTo know\n\t\t\t\tWhere\n\t\t\t\t\tDoes\n\t\t\t\t\t\tPheasant sit\n\n";
+  std::cout<<"Задание 3\n\n";
+  std::cout<<"\t\t\tПродам щенков.\n\t\t\tПорода:Бульдог.\n\t\t\tТел:132-12-34\n\t\t\t132\t\t|\t132\t|\n\t\t\t12\t\t|\t12\t|\n\t\t\t34\t\t|\t34\t|";
+}
