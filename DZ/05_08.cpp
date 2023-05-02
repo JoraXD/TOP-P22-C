243ae4209cdf56913b9a0ef68019542dfcee0fc4
Георгий Беляев <123897400+JoraXD@users.noreply.github.com>
2023-05-02 19:00:35 +0000

    05_08

--- DZ/05_08.cpp
+++ DZ/05_08.cpp
@@ -1,20 +1,102 @@
 #include <iostream>
-int min(int a,int b,int c,int d) {
-    if(a>b&a>c&a>d){
-        return a;
-    }
-    if(b>a&b>c&b>d){
-        return b;
-    }
-    if(c>b&c>a&c>d){
-        return c;
-    }
-    if(d>b&d>c&d>a){
-        return d;
-    }
-}
+//int min(int a,int b,int c,int d) {
+//    if(a>b&a>c&a>d){
+//        return a;
+//    }
+//    if(b>a&b>c&b>d){
+//        return b;
+//    }
+//    if(c>b&c>a&c>d){
+//        return c;
+//    }
+//    if(d>b&d>c&d>a){
+//        return d;
+//    }
+//}
+//
+//double power(double a,int n){
+//    int num=1;
+//    for(int i=1;i<n+1;i++){
+//        num*=a;
+//    }
+//    return num;
+//}
+//
+// bool Xor (bool x, bool y){
+//        if(x==true&y!=true){
+//            return true;
+//        }
+//        if(x!=true&y==true){
+//            return true;
+//        }
+//    }
+//
+//bool Election(bool x, bool y, bool z){
+//    int countz=0,counto=0;
+//    bool arr[3]{x,y,z};
+//    for(int i=0;i<3;++i){
+//        if(arr[i]==true){
+//            counto++;
+//        }
+//        else{
+//            countz++;
+//        }
+//    }
+//    if (countz>counto){
+//        return false;
+//    }
+//    else{
+//        return true;
+//    }
+//}
+//
+//bool IsPrime (int n){
+//    for (int i = 2; i < n; i++){
+//            if (n % i == 0) {
+//                return false;
+//            }
+//            return true;
+//    }
+//}
+//
+//int step(int a,int n){
+//    if(n>0){
+//        return a*step(a,n-1);
+//    }
+//    else if(n==0){
+//        return 1;
+//    }
+//}
+
+
+
 int main(){
-    int a,b,c,d;
-    std::cin>>a>>b>>c>>d;
-    min(a,b,c,d);
+    //std::cout<< "Задание 1\n";
+    //int a,b,c,d;
+    //std::cin>>a>>b>>c>>d;
+    //std::cout<<min(a,b,c,d)<<"\n";
+    //std::cout<<"Задание 2\n";
+    //double a1;
+    //int n;
+    //std::cin>>a1>>n;
+    //std::cout<<power(a1,n)<<"\n";
+    //std::cout<<"Задание 3\n";
+    //bool x,y;
+    //std::cin>> x>>y;
+    //std::cout<<Xor(x,y)<<"\n";
+    //std::cout<<"Задание 4\n";
+    //bool x,y,z;
+    //std::cin >>x>>y>>z;
+    //std::cout<<Election(x,y,z)<<"\n";
+    //std::cout<<"Задание 5\n";
+    //int n;
+    //std::cin>>n;
+    //if(IsPrime(n)==1){
+    //    std::cout<<"prime\n";
+    //}
+    //else{
+    //    std::cout<<"composite\n";
+    //}
+    //std::cout<<"Задание 6\n";
+    //std::cout<<step(2,5)<<"\n";
 }
